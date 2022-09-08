243e13531ea802edd71dd6f079504d1074ab160e
Alex Parsons <alex.parsons@mysociety.org>
2022-09-08 15:02:23 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
 # mysoc-dataset
 
-Tool to download mySociety datasets
+A python package and CLI to download mySociety datasets.
+
+## Installation
+
+It can be installed with:
+
+```
+python -m pip install mysoc-dataset
+```
+
+or if using poetry:
+
+```
+poetry add mysoc-dataset
+```
+
+## Usage
+
+### As a package
+
+The package has two basic functions (with helpful error messages) to access the url or a dataframe
+of the resource.
+
+```python
+from mysoc_dataset import get_dataset_url, get_dataset_df
+
+url = get_dataset_url(
+    repo_name="uk_local_authority_names_and_codes",
+    package_name="uk_la_future",
+    version_name="latest",
+    file_name="uk_local_authorities_future.csv",
+)
+
+# get a pandas dataframe
+df = get_dataset_df(
+    repo_name="uk_local_authority_names_and_codes",
+    package_name="uk_la_future",
+    version_name="latest",
+    file_name="uk_local_authorities_future.csv",
+)
+
+
+```
+
+### As a CLI
+
+The CLI can be used to explore avaliable data using the `list` command, get the [frictionless datapackage](https://frictionlessdata.io/) that describes the repo using `detail` fetch the url with the `url` command or download the file using `download`.
+
+This can be used to source files or pipe the URLs into other functions without writing python scripts. 
+
+The CLI can either be run as `python -m mysoc_dataset` or `mysoc-dataset`. 
+
+For instance, the following will print the `datapackage.json` that describes the underlying contents. 
+
+`mysoc-dataset detail --repo uk_local_authority_names_and_codes --version latest --package uk_la_future`
+
+And the following will get the URL of the resource:
+
+`mysoc-dataset url --repo uk_local_authority_names_and_codes --version latest --package uk_la_future --file uk_local_authorities_future.csv`
+
+Use `mysoc-dataset --help` for more instructions. 
+
+If using the CLI for a dataset, please fill out a survey of what you are using it for to help us explain the value of the data to funders. You can get a URL to the survey page using the 'survey' command. 
+
+`mysoc-dataset survey --repo uk_local_authority_names_and_codes --version latest --package uk_la_future --file uk_local_authorities_future.csv`
--- tests/test_mysoc_dataset.py
+++ tests/test_mysoc_dataset.py
@@ -1,5 +1,145 @@
-import mysoc_dataset as package
+from typing import Any
+
+import pytest
+from mysoc_dataset import get_dataset_df, get_dataset_url, get_public_datasets
+from mysoc_dataset.dataset import (
+    FileNotFound,
+    PackageNotFound,
+    RepoNotFound,
+    VersionNotFound,
+)
 
 
-def test_true_is_true():
-    assert True is True
+def test_public_dataset_fetch_works():
+    datasets = get_public_datasets()
+    assert "https://mysociety.github.io/uk_local_authority_names_and_codes" in datasets
+
+
+def test_get_dataset_url_works():
+    url = get_dataset_url(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "latest",
+        "uk_local_authorities_future.csv",
+    )
+    assert "uk_la_future" in url
+    assert ".csv" in url
+    assert "latest" in url
+
+
+def test_full_url_works():
+    url = get_dataset_url(
+        "https://mysociety.github.io/uk_local_authority_names_and_codes/",
+        "uk_la_future",
+        "latest",
+        "uk_local_authorities_future.csv",
+    )
+    assert "uk_la_future" in url
+    assert ".csv" in url
+    assert "latest" in url
+
+
+def test_non_existant_file_raises_error():
+    with pytest.raises(FileNotFound):
+        get_dataset_url(
+            "uk_local_authority_names_and_codes",
+            "uk_la_future",
+            "latest",
+            "uk_local_authorities_future_nope.csv",
+        )
+
+
+def test_non_existant_version_raises_error():
+    with pytest.raises(VersionNotFound):
+        get_dataset_url(
+            "uk_local_authority_names_and_codes",
+            "uk_la_future",
+            "b-latest",
+            "uk_local_authorities_future_.csv",
+        )
+
+
+def test_non_existant_package_raises_error():
+    with pytest.raises(PackageNotFound):
+        get_dataset_url(
+            "uk_local_authority_names_and_codes",
+            "uk_la_future-d",
+            "b-latest",
+            "uk_local_authorities_future_.csv",
+        )
+
+
+def test_non_existant_repo_raises_error():
+    with pytest.raises(RepoNotFound):
+        get_dataset_url(
+            "uk_local_authority_names_and_codes_blah",
+            "uk_la_future-d",
+            "b-latest",
+            "uk_local_authorities_future_.csv",
+        )
+
+
+def test_survey_message(capsys: Any):
+    get_dataset_url(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "latest",
+        "uk_local_authorities_future.csv",
+    )
+    assert "can you tell us how using this survey" in capsys.readouterr().out
+
+
+def test_survey_message_can_be_turned_off(capsys: Any):
+    get_dataset_url(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "latest",
+        "uk_local_authorities_future.csv",
+        done_survey=True,
+    )
+    assert "can you tell us how using this survey" not in capsys.readouterr().out
+
+
+def test_version_message(capsys: Any):
+    get_dataset_url(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "0.1.0",
+        "uk_local_authorities_future.csv",
+        done_survey=True,
+    )
+    assert "is not the latest version" in capsys.readouterr().out
+
+
+def test_version_message_can_be_turned_off(capsys: Any):
+    get_dataset_url(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "0.1.0",
+        "uk_local_authorities_future.csv",
+        done_survey=True,
+        ignore_version_warning=True,
+    )
+    assert "is not the latest version" not in capsys.readouterr().out
+
+
+def test_no_version_message_for_latest(capsys: Any):
+    get_dataset_url(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "latest",
+        "uk_local_authorities_future.csv",
+        done_survey=True,
+    )
+    assert "is not the latest version" not in capsys.readouterr().out
+
+
+def test_dataframe_load_works_ok():
+    df = get_dataset_df(
+        "uk_local_authority_names_and_codes",
+        "uk_la_future",
+        "latest",
+        "uk_local_authorities_future.csv",
+    )
+    assert len(df) > 0
+    assert "local-authority-code" in df.columns
